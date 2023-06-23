ef24f75ff3224c394ac90ab56b930d3f354f27bd
Annetta777 <loginaanna2000a@gmail.com>
2023-06-23 18:42:07 +0600

    добавлена верстка виджетта

--- scripts/modules/utils.js
+++ scripts/modules/utils.js
@@ -1,3 +1,9 @@
+const addZero = (n) => {
+  if (n < 10) {
+    n = `0${n}`
+  }
+  return n
+}
 export const getCurrentDateTime = () => {
 const months = [
   'янв', 
@@ -30,16 +36,8 @@ const month = months[date.getMonth()]
 const year = date.getFullYear()
 const dayOfWeek = weekDays[date.getDay()]
 
-let hours = date.getHours()
-let minutes = date.getMinutes()
-
-if(hours < 10) {
-  hours = `0${hours}`
-}
-
-if(minutes < 10) {
-  minutes = `0${minutes}`
-}
+const hours = addZero(date.getHours())
+const minutes = addZero(date.getMinutes())
 
 return {dayOfMonth, month, year, hours, minutes, dayOfWeek}
 
